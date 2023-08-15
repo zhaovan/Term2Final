226b2d2916acae3dd1bb50dbc5ab0dd02812dfc5
Ivan Zhao <ivanzhao8@gmail.com>
2023-08-14 17:49:19 -0700

    finish type version of it

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import { LocomotiveScrollProvider } from "react-locomotive-scroll";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import { useLocomotiveScroll } from "react-locomotive-scroll";
 
@@ -14,7 +14,17 @@ const text = [
   "that would be sick",
   "instead...",
   "im making a specimen at 4am in the morning",
+  "here are some glyphs",
+  "ABCDEFGHIJKLM ",
+  "NOPQRST",
+  "UVWXYZ",
   "this is the part of it where I slowly went insane",
+  "!!!!!",
+  "&&&&",
+  ".... @@@@@",
+  "okay but the lowercase k????",
+  "if you hate it don't tell me",
+  "I am sooooo in love with slamming my face into the keyboard",
   "in case you weren't aware I'm a huge procrasinator",
   "honestly... I think all designers are procrastinators",
   "like the fact that you're expected to tap into creativity on demand is wild",
@@ -47,29 +57,35 @@ const text = [
 export default function Home() {
   const containerRef = useRef(null);
   const { scroll } = useLocomotiveScroll();
+  const [glyphs, setGlyphs] = useState(false);
   return (
     <LocomotiveScrollProvider options={{ smooth: true }}>
       <main data-scroll-container ref={containerRef}>
         <div className="flex justify-center items-center h-screen">
-          <div className="text-center flex flex-col gap-4">
+          <div className="text-center flex flex-col gap-4 mx-32">
             <h1 className="text-7xl">Convo</h1>
             <h2 className="text-2xl">
               What happens when you give an engineer too much choice and the
               Robofont scripting window when asking him to design a typeface
             </h2>
             <h2>A typeface project by Ivan Zhao for Term 2</h2>
+            <p className="font-bold">(send help and scroll down)</p>
           </div>
         </div>
-        {text.map((currText, i) => {
-          return (
-            <div
-              className="h-screen flex justify-center items-center text-center mx-24 py-4"
-              key={i}
-            >
-              <p className="text-7xl">{currText}</p>
-            </div>
-          );
-        })}
+        {glyphs ? (
+          <div></div>
+        ) : (
+          text.map((currText, i) => {
+            return (
+              <div
+                className="h-screen flex justify-center items-center text-center mx-24 py-4 font-normal"
+                key={i}
+              >
+                <p className="text-7xl">{currText}</p>
+              </div>
+            );
+          })
+        )}
       </main>
     </LocomotiveScrollProvider>
   );
